bc764f7a90854b0021fb08a527706601de6e49c6
Aleja <71413849+aleja0923@users.noreply.github.com>
2020-10-21 14:42:00 -0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,72 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+	public GameObject turret;
+	public Vector3 positionOffset;
+	public Color hoverColor;
+	private Color startColor;
+	public Color notEnoughMoneyColor;
+
+	private Renderer rend;
+	BuilderManager builderManager;
+	public TurretBluePrint turretBluePrint;
+	
+	 void Start()
+	{
+		rend = GetComponent<Renderer>();
+		startColor = rend.material.color;
+		builderManager = BuilderManager.instance;
+	}
+
+	public Vector3 GetBuildPosition()
+	{
+		return transform.position + positionOffset;  
+	}
+
+	 void OnMouseDown()
+	{
+		if (!builderManager.canBuild)
+			return;
+
+		if (turret != null)
+		{
+			builderManager.SelectedNode(this);
+				return;
+		}
+
+		builderManager.BuildTurretOn(this);
+	}
+
+	public void SellTurret()
+	{
+		PlayerStats.Money += turretBluePrint.GetSellAmount();
+
+		Destroy(turret);
+		turretBluePrint = null;
+	}
+
+	void OnMouseEnter()
+	{
+
+		if (!builderManager.canBuild) 
+			return;
+
+		if (builderManager.HasMoney)
+		{
+			rend.material.color = hoverColor;
+		}
+		else
+		{
+			rend.material.color = notEnoughMoneyColor;
+		}
+		
+	}
+
+	 void OnMouseExit()
+	{
+		rend.material.color = startColor;
+	}
+}
