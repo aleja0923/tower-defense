1526f25acd6ed666b88c915abb0041f031bc0b8e
Aleja <71413849+aleja0923@users.noreply.github.com>
2020-10-21 14:39:54 -0500

    Add files via upload

---
+++ Win.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+
+	public void Retry()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void Menu()
+	{
+		Debug.Log("ir al menu");
+	}
+}
+
+
