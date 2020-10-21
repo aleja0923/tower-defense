957c65dc27b5176a17e81013e5b61a6ebf7091dc
Aleja <71413849+aleja0923@users.noreply.github.com>
2020-10-21 14:41:10 -0500

    Add files via upload

---
+++ TurretBluePrint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TurretBluePrint
+{
+	public GameObject prefab;
+	public int cost;
+
+	public int GetSellAmount()
+	{
+		return cost/2;
+	}
+}
